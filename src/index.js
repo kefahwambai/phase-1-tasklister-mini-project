23867122ba2b5618fd4df8d3d215cff6f0688ef7
KefahWambai <kefah.ngwei@student.moringaschool.com>
2022-12-04 13:14:13 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded",  () => {
   // your code here
+  const newForm = document.getElementById('create-task-form');
+  
+  newForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const newTask = document.getElementById("new-task-description");
+    const taskList = document.getElementById("tasks");
+    const newTaskItem = document.createElement("li");
+    newTaskItem.innerText = newTask.value;
+    taskList.appendChild(newTaskItem);
+   
+    event.target.reset();
+  });
 });
+  
+  
+ 
